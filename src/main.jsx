70b7f3a4a8a7a4476551b1dadb0ed08ceab464cf
Pranjal500 <mrpranjal917@gmail.com>
2026-02-23 21:47:06 +0530

    redux code Added

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import {AppProvider} from "./context/AppContext"
+import {AppProvider} from "./context/AppContext";
+import {Provider} from "react-redux";
+import {store} from "./store/Store";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <AppProvider>
     <App />
     </AppProvider>
+    </Provider>
   </StrictMode>,
 )
